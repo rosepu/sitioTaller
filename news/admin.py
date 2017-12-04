72eb856018aa7ce534107cd20423926d11bad954
Rodrigo Sepúlveda <rodrigo.sepulveda.12@sansano.usm.cl>
2017-12-04 19:03:10 -0300

    news admid + gallery

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from .models import New, Category, NewGallery
 
-admin.site.register(New)
+
+class GalleryInline(admin.StackedInline):
+	model = NewGallery
+	extra = 1
+
+class NewAdmin(admin.ModelAdmin):
+	list_display = ('title', 'date', 'new_type')
+	inlines = []
+	
+	def get_form(self, request, obj=None, **kwargs):
+		if (obj == None or obj.new_type ==  New.GALLERY_TYPE): 
+			self.inlines = [GalleryInline]
+		return super(NewAdmin, self).get_form(request, obj, **kwargs)
+
+admin.site.register(New, NewAdmin)
 admin.site.register(Category)
 admin.site.register(NewGallery)
