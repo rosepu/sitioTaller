2e71924bae98b49cdf66dbb883df4cad9cef3305
Rodrigo Sepúlveda <rodrigo.sepulveda.12@sansano.usm.cl>
2017-12-07 02:11:52 -0300

    add category view, and improvement views

--- news/views.py
+++ news/views.py
@@ -1,11 +1,13 @@
 from django.http      import HttpResponse
 from django.shortcuts import get_object_or_404,render
-from .models          import New, NewGallery, Carousel
+from .models          import New, NewGallery, Carousel, Category
 
 def index(request):
 	new_list = New.objects.order_by('-date')
+	category_list = Category.objects.order_by('name')
 	context = {
-		'new_list': new_list
+		'new_list': new_list,
+		'category_list': category_list,
 	}
 	return render(request, 'news/index.html', context)
 
@@ -32,3 +34,13 @@ def home(request):
 		'carousel': carousel_home,
 	}
 	return render(request, 'news/home.html', context)
+
+def category_index(request, category_slug):
+	category_item = Category.objects.get(slug=category_slug)
+	new_list = New.objects.filter(category=category_item).order_by('-date')
+	category_list = Category.objects.order_by('name')
+	context = {
+		'new_list': new_list,
+		'category_list': category_list,
+	}
+	return render(request, 'news/index.html', context)
